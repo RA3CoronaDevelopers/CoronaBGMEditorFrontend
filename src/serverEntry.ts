a674d26a886fd1cc32c86a94256cb71a16ba187c
langyo <langyo.china@gmail.com>
2021-05-03 15:16:37 +0800

    ⚡️ 大幅改善 WebSocket 数据交换模块的接口，使数据接收函数无需单独编写。

--- src/serverEntry.ts
+++ src/serverEntry.ts
@@ -8,16 +8,16 @@ export { };
 import { diskinfo } from '@dropb/diskinfo';
 import {
   readdirSync, mkdirSync,
-  statSync, existsSync, readFileSync, writeFileSync } from 'fs';
+  statSync, existsSync, readFileSync, writeFileSync
+} from 'fs';
 import { join } from 'path';
 
 const middlewares: {
-  [type: string]: (data: any) => Promise<void>
+  [type: string]: (data: any) => Promise<any>
 } = {
   // 文件选取器窗口的工具函数
   async getProcessDir(_data) {
-    send({
-      type: 'setFileSelectorDir',
+    return {
       path: process.cwd(),
       dirContent: (readdirSync(process.cwd())).reduce((obj, name) => ({
         ...obj,
@@ -29,11 +29,10 @@ const middlewares: {
           }
         })()
       }), {})
-    });
+    };
   },
   async getPreviousDir({ path }) {
-    send({
-      type: 'setFileSelectorDir',
+    return {
       path: join(path, '../'),
       dirContent: (readdirSync(join(path, '../'))).reduce((obj, name) => ({
         ...obj,
@@ -45,11 +44,10 @@ const middlewares: {
           }
         })()
       }), {})
-    });
+    };
   },
   async getDir({ path }) {
-    send({
-      type: 'setFileSelectorDir',
+    return {
       path: join(path),
       dirContent: (readdirSync(join(path))).reduce((obj, name) => ({
         ...obj,
@@ -61,96 +59,86 @@ const middlewares: {
           }
         })()
       }), {})
-    });
+    };
   },
   async getDiskList() {
-    send({
-      type: 'setDiskList',
+    return {
       disks: (await diskinfo()).map(n => join(n.target + '/'))
-    })
+    };
   },
   async createFile({ path, name, initContent }) {
     if (existsSync(join(path, name))) {
-      send({
-        type: 'createFileCallback',
+      return {
         hasSuccess: false,
         reason: '文件已存在'
-      });
+      };
     } else {
       try {
         writeFileSync(join(path, name), initContent);
-      } catch(e) {
-        send({
-          type: 'createFileCallback',
+      } catch (e) {
+        return {
           hasSuccess: false,
           reason: `${e}`
-        });
-      } finally {
-        send({
-          type: 'createFileCallback',
-          hasSuccess: true
-        });
-        send({
-          type: 'setFileSelectorDir',
-          path: join(path),
-          dirContent: (readdirSync(join(path))).reduce((obj, name) => ({
-            ...obj,
-            [name]: (() => {
-              try {
-                return statSync(join(path, name)).isDirectory() ? 'dir' : 'file';
-              } catch (_e) {
-                return 'file';
-              }
-            })()
-          }), {})
-        });
+        };
       }
+      return {
+        hasSuccess: true,
+        dirContent: (readdirSync(join(path))).reduce((obj, name) => ({
+          ...obj,
+          [name]: (() => {
+            try {
+              return statSync(join(path, name)).isDirectory() ? 'dir' : 'file';
+            } catch (_e) {
+              return 'file';
+            }
+          })()
+        }), {})
+      };
     }
   },
   async createFolder({ path, name }) {
     if (existsSync(join(path, name))) {
-      send({
-        type: 'createFolderCallback',
+      return {
         hasSuccess: false,
         reason: '文件夹已存在'
-      });
+      };
     } else {
       try {
         mkdirSync(join(path, name));
-      } catch(e) {
-        send({
-          type: 'createFolderCallback',
+      } catch (e) {
+        return {
           hasSuccess: false,
           reason: `${e}`
-        });
-      } finally {
-        send({
-          type: 'createFolderCallback',
-          hasSuccess: true
-        });
-        send({
-          type: 'setFileSelectorDir',
-          path: join(path),
-          dirContent: (readdirSync(join(path))).reduce((obj, name) => ({
-            ...obj,
-            [name]: (() => {
-              try {
-                return statSync(join(path, name)).isDirectory() ? 'dir' : 'file';
-              } catch (_e) {
-                return 'file';
-              }
-            })()
-          }), {})
-        });
+        };
       }
+      return {
+        hasSuccess: true,
+        dirContent: (readdirSync(join(path))).reduce((obj, name) => ({
+          ...obj,
+          [name]: (() => {
+            try {
+              return statSync(join(path, name)).isDirectory() ? 'dir' : 'file';
+            } catch (_e) {
+              return 'file';
+            }
+          })()
+        }), {})
+      };
     }
   }
 };
+
 receive((msg: any) => {
   if (!msg.type || !middlewares[msg.type]) {
-    console.error('WS Unknown type', msg);
+    console.error('WS Unknown type', msg.type);
+  } else if (!msg.id) {
+    console.error('WS Non id', msg.type);
   } else {
-    console.log('WS Trigger', msg.type);
-    middlewares[msg.type](msg);
+    console.log('WS Trigger', msg.type, msg.id);
+    middlewares[msg.type](msg.data).then(data => send({
+      type: msg.type,
+      id: msg.id,
+      data
+    }));
   }
 });
