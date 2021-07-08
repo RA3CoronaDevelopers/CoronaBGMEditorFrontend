0908887b7fc2cf2cc93f2def65e1abd06e0b62d3
伊欧 <langyo.china@gmail.com>
2021-07-08 20:09:19 +0800

    💄 为检查点窗口加入了新增跳转位置与新增条件的功能。

--- src/components/dialogs/checkPointController.tsx
+++ src/components/dialogs/checkPointController.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useRef, useEffect, useState } from 'react';
 import {
   Typography,
   Button,
+  IconButton,
   List,
   ListItem,
   TextField,
@@ -10,9 +11,12 @@ import {
   MenuItem,
   InputLabel,
   Paper,
-  Grid
+  Grid, GridSize,
+  Divider
 } from '@material-ui/core';
 import { css } from '@emotion/css';
+import { Icon } from '@mdi/react';
+import { mdiClose, mdiPlus } from '@mdi/js';
 import { StoreContext } from '../../utils/storeContext';
 import { ICheckPoint } from '../../utils/jsonConfigTypes';
 import { drawWaveform } from '../../utils/waveformDrawer';
@@ -224,32 +228,112 @@ export function CheckPointController({
         {checkPoint?.destinations && checkPoint?.destinations.map(({ condition, jumpTo }, destinationId) => <ListItem>
           <Paper>
             <List>
+              <div
+                className={css`
+                  margin: 0px 16px;
+                `}
+              >
+                <TextField
+                  variant='outlined'
+                  value={condition}
+                  onChange={e => onChange({
+                    ...checkPoint,
+                    destinations: [
+                      ...checkPoint.destinations.slice(0, destinationId),
+                      {
+                        condition: e.target.value,
+                        jumpTo: checkPoint.destinations[destinationId].jumpTo
+                      },
+                      ...checkPoint.destinations.slice(destinationId + 1)
+                    ]
+                  })}
+                  fullWidth
+                />
+              </div>
               {jumpTo.map((jumpToData, jumpToId) => <ListItem>
                 <Grid container spacing={1}>
                   <Grid item xs={12}>
-                    <TextField
-                      variant='outlined'
-                      value={condition}
-                      onChange={e => onChange({
-                        ...checkPoint,
-                        destinations: [
-                          ...checkPoint.destinations.slice(0, destinationId),
-                          {
-                            condition: e.target.value,
-                            jumpTo: checkPoint.destinations[destinationId].jumpTo
-                          },
-                          ...checkPoint.destinations.slice(destinationId + 1)
-                        ]
-                      })}
-                      fullWidth
-                    />
+                    <Divider />
                   </Grid>
-                  <Grid item xs={4}>
-                    <FormControl variant='outlined' fullWidth>
-                      <InputLabel>目标轨道</InputLabel>
-                      <Select
-                        value={(console.log(jumpToData.targetTrackId), jumpToData.targetTrackId)}
-                        onChange={e => onChange({
+                  <Grid item xs={11}>
+                    <Grid container spacing={1}>
+                      <Grid item xs={8}>
+                        <FormControl variant='outlined' fullWidth>
+                          <InputLabel>目标轨道</InputLabel>
+                          <Select
+                            value={jumpToData.targetTrackId}
+                            onChange={e => onChange({
+                              ...checkPoint,
+                              destinations: [
+                                ...checkPoint.destinations.slice(0, destinationId),
+                                {
+                                  condition: checkPoint.destinations[destinationId].condition,
+                                  jumpTo: [
+                                    ...checkPoint.destinations[destinationId].jumpTo.slice(0, jumpToId),
+                                    {
+                                      ...checkPoint.destinations[destinationId].jumpTo[jumpToId],
+                                      targetTrackId: e.target.value as string
+                                    },
+                                    ...checkPoint.destinations[destinationId].jumpTo.slice(jumpToId + 1)
+                                  ]
+                                },
+                                ...checkPoint.destinations.slice(destinationId + 1)
+                              ]
+                            })}
+                            label="目标轨道"
+                          >
+                            {tracks.map(({ name }) => <MenuItem value={name}>{name}</MenuItem>)}
+                          </Select>
+                        </FormControl>
+                      </Grid>
+                      {[
+                        { type: 'targetOffset', label: '目标轨道位置(s)', xs: 4 },
+                        { type: 'fadeOutDelay', label: '渐出延迟时间(s)', xs: 3 },
+                        { type: 'fadeOutDuration', label: '渐出持续时间(s)', xs: 3 },
+                        { type: 'targetFadeInDelay', label: '渐入延迟时间(s)', xs: 3 },
+                        { type: 'targetFadeInDuration', label: '渐入持续时间(s)', xs: 3 }
+                      ].map(({ type, label, xs }) => <Grid item xs={xs as GridSize}>
+                        <TextField
+                          variant='outlined'
+                          value={jumpToData[type]}
+                          label={label}
+                          type='number'
+                          onChange={e => onChange({
+                            ...checkPoint,
+                            destinations: [
+                              ...checkPoint.destinations.slice(0, destinationId),
+                              {
+                                condition: checkPoint.destinations[destinationId].condition,
+                                jumpTo: [
+                                  ...checkPoint.destinations[destinationId].jumpTo.slice(0, jumpToId),
+                                  {
+                                    ...checkPoint.destinations[destinationId].jumpTo[jumpToId],
+                                    [type]: e.target.value
+                                  },
+                                  ...checkPoint.destinations[destinationId].jumpTo.slice(jumpToId + 1)
+                                ]
+                              },
+                              ...checkPoint.destinations.slice(destinationId + 1)
+                            ]
+                          })}
+                          fullWidth
+                        />
+                      </Grid>)}
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <div
+                      className={css`
+                          width: 100%;
+                          height: 100%;
+                          display: flex;
+                          justify-content: center;
+                          align-items: center;
+                      `}
+                    >
+                      <IconButton
+                        size='small'
+                        onClick={() => onChange({
                           ...checkPoint,
                           destinations: [
                             ...checkPoint.destinations.slice(0, destinationId),
@@ -257,60 +341,100 @@ export function CheckPointController({
                               condition: checkPoint.destinations[destinationId].condition,
                               jumpTo: [
                                 ...checkPoint.destinations[destinationId].jumpTo.slice(0, jumpToId),
-                                {
-                                  ...checkPoint.destinations[destinationId].jumpTo[jumpToId],
-                                  targetTrackId: e.target.value as string
-                                },
                                 ...checkPoint.destinations[destinationId].jumpTo.slice(jumpToId + 1)
                               ]
                             },
                             ...checkPoint.destinations.slice(destinationId + 1)
                           ]
                         })}
-                        label="目标轨道"
                       >
-                        {tracks.map(({ name }) => <MenuItem value={name}>{name}</MenuItem>)}
-                      </Select>
-                    </FormControl>
+                        <Icon path={mdiClose} size={1} color='#fff' />
+                      </IconButton>
+                    </div>
                   </Grid>
-                  {[
-                    { type: 'fadeOutDelay', label: '渐出延迟时间(s)' },
-                    { type: 'targetFadeInDelay', label: '渐入延迟时间(s)' },
-                    { type: 'targetOffset', label: '目标轨道位置(s)' },
-                    { type: 'fadeOutDuration', label: '渐出持续时间(s)' },
-                    { type: 'targetFadeInDuration', label: '渐入持续时间(s)' }
-                  ].map(({ type, label }) => <Grid item xs={4}>
-                    <TextField
-                      variant='outlined'
-                      value={jumpToData[type]}
-                      label={label}
-                      type='number'
-                      onChange={e => onChange({
-                        ...checkPoint,
-                        destinations: [
-                          ...checkPoint.destinations.slice(0, destinationId),
-                          {
-                            condition: checkPoint.destinations[destinationId].condition,
-                            jumpTo: [
-                              ...checkPoint.destinations[destinationId].jumpTo.slice(0, jumpToId),
-                              {
-                                ...checkPoint.destinations[destinationId].jumpTo[jumpToId],
-                                [type]: e.target.value
-                              },
-                              ...checkPoint.destinations[destinationId].jumpTo.slice(jumpToId + 1)
-                            ]
-                          },
-                          ...checkPoint.destinations.slice(destinationId + 1)
-                        ]
-                      })}
-                      fullWidth
-                    />
-                  </Grid>)}
                 </Grid>
               </ListItem>)}
+              <div
+                className={css`
+                  width: 100%;
+                  display: flex;
+                  justify-content: center;
+                `}
+              >
+                <Button
+                  onClick={() => onChange({
+                    ...checkPoint,
+                    destinations: [
+                      ...checkPoint.destinations.slice(0, destinationId),
+                      {
+                        ...checkPoint.destinations[destinationId],
+                        jumpTo: [
+                          ...checkPoint.destinations[destinationId].jumpTo,
+                          {
+                            targetTrackId: tracks[0].name,
+                            targetOffset: 0,
+                            fadeOutDelay: 0,
+                            fadeOutDuration: 0,
+                            targetFadeInDelay: 0,
+                            targetFadeInDuration: 0
+                          }
+                        ]
+                      },
+                      ...checkPoint.destinations.slice(destinationId + 1)
+                    ]
+                  })}
+                >
+                  <Icon path={mdiPlus} size={1} />
+                  <div
+                    className={css`
+                      width: 8px;
+                    `}
+                  />
+                  {'新建跳转目标'}
+                </Button>
+              </div>
             </List>
           </Paper>
         </ListItem>)}
+        <ListItem>
+          <div
+            className={css`
+              width: 100%;
+              display: flex;
+              justify-content: center;
+            `}
+          >
+            <Button
+              onClick={() => onChange({
+                ...checkPoint,
+                destinations: [
+                  ...checkPoint.destinations,
+                  {
+                    condition: '',
+                    jumpTo: [
+                      {
+                        targetTrackId: tracks[0].name,
+                        targetOffset: 0,
+                        fadeOutDelay: 0,
+                        fadeOutDuration: 0,
+                        targetFadeInDelay: 0,
+                        targetFadeInDuration: 0
+                      }
+                    ]
+                  }
+                ]
+              })}
+            >
+              <Icon path={mdiPlus} size={1} />
+              <div
+                className={css`
+                  width: 8px;
+                `}
+              />
+              {'新建检查点'}
+            </Button>
+          </div>
+        </ListItem>
       </List>
     </DialogBase>
   );
